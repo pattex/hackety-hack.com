9282dd29eb2b4c3ff0a4333fb81f412a86f879cf
Arne Eilermann <eilermann@lavabit.com>
2012-03-22 00:02:00 +0100

    Simply set up the localization. Pathes like "/de/questions" would be nicer but this is the less invasive way to do it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,17 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to login_url, :alert => exception.message
   end
-  
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
+
+  before_filter :set_locale
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
 end
